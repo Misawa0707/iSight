6f7975af51ce51332316605e733503141ba0ebaa
misawa <guiyusanze40@gmail.com>
2018-04-18 09:38:49 +0900

    no message

---
+++ Assets/trap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trap : MonoBehaviour {
+
+    public GameObject Wall;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Wall.SetActive(false);
+        }
+    }
+}
