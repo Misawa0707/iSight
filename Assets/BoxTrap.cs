fa0e56f17c9a4cf111b21448e0652edec6b7e37f
misawa <guiyusanze40@gmail.com>
2018-04-26 13:40:59 +0900

    調整

---
+++ Assets/BoxTrap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxTrap : MonoBehaviour {
+
+    public GameObject Wall;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "MoveBox")
+        {
+            Debug.Log("ひらく");
+            Wall.SetActive(false);
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "MoveBox")
+        {
+            Debug.Log("とじる");
+            Wall.SetActive(true);
+        }
+    }
+}
---
+++ Assets/key.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class key : MonoBehaviour
+{
+
+    public GameObject Wall;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "keyBox")
+        {
+            Debug.Log("ひらく");
+            Wall.SetActive(true);
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "keyBox")
+        {
+            Debug.Log("とじる");
+            Wall.SetActive(false);
+        }
+    }
+}
