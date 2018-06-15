9ded925be92f0d39f98508b9251dc60b11b76748
misawa <guiyusanze40@gmail.com>
2018-06-15 10:11:21 +0900

    アイテムと扉との判定

--- Assets/Script/trap.cs
+++ Assets/Script/trap.cs
@@ -1,27 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class trap : MonoBehaviour
 {
     
     public GameObject door;
     public ItemManager itemkey;
-  
+    public GameObject text;
+    bool Textflag = false;
+    float time;
+    float timeMax = 60.0f;
     void Start()
     {
         itemkey = itemkey.GetComponent<ItemManager>();
     }
-        void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
-        if (itemkey.GetItemFlag(ItemManager.Item.Key)==true)
+      　//プレイヤーと当たっていたら
+        if (other.gameObject.tag == "Player")
+        { //鍵を持っていたら   
+            if (itemkey.GetItemFlag(ItemManager.Item.Key) == true)
+            {
+                   door.SetActive(false);
+            }
+            else
+            {
+                text.SetActive(true);
+                time =0;
+                Textflag = true;
+            }
+        }
+    }
+    void Update()
+    {
+        if(Textflag==true)
         {
-              if (other.gameObject.tag == "Player")
-             {
-                   Debug.Log("ひらく");
-               door.SetActive(false);
-             }
-
+            time += 1;
+            if (time > timeMax)
+            {
+                text.SetActive(false);
+                Textflag = false;
+            }
         }
     }
 }
